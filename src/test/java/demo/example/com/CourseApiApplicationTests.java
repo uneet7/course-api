eb426bca5883f7d5949d3a2b75233d10adc27a86
Uneet Patel <uneet.patel@joinsage.com>
2020-06-10 22:33:05 +0530

    Feat: Cruds end points

---
+++ src/test/java/demo/example/com/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.example.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
